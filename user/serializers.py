a1ed3aa6abfed0e0d4fda0afa87a4501958011c5
NEMANICBOOK\shins <shinsung2002@gmail.com>
2025-02-05 11:23:43 +0900

    수정 2

--- user/serializers.py
+++ user/serializers.py
@@ -10,11 +10,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'nickname', 'email']
 
 class SignupSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(write_only=True, required=True)
-
     class Meta:
         model = User
-        fields = ['username', 'password', 'password2', 'nickname', 'email']
+        fields = ['username', 'password', 'nickname', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate_nickname(self, value):
@@ -29,14 +27,7 @@ class SignupSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("이미 등록된 이메일입니다.")
         return value
 
-    def validate(self, data):
-        """ 비밀번호 확인 검증 """
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError({"password": "비밀번호가 일치하지 않습니다."})
-        return data
-
     def create(self, validated_data):
         """ 비밀번호 해싱 후 저장 """
-        validated_data.pop('password2')  # password2는 저장할 필요 없음
-        validated_data['password'] = make_password(validated_data['password'])  # 비밀번호 해싱
+        validated_data['password'] = make_password(validated_data['password'])  # ✅ 비밀번호 해싱
         return User.objects.create(**validated_data)
